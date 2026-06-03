1b737816c5335f2a97bbf15f644394154657cce0
Bonaldo21 <cauakingg@gmail.com>
2026-06-03 20:46:48 -0300

    ligando ao root tudo

--- primordi_frontend/nextjs_space/next.config.js
+++ primordi_frontend/nextjs_space/next.config.js
@@ -1,27 +1,20 @@
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE || 'standalone',
-  productionBrowserSourceMaps: false,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.filename = 'static/chunks/[name]-[contenthash:8].js';
-      config.output.chunkFilename = 'static/chunks/[contenthash:16].js';
-    }
-    return config;
-  },
+    output: 'standalone', // fixo, sem variável de ambiente
+    productionBrowserSourceMaps: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+    images: { unoptimized: true },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.output.filename = 'static/chunks/[name]-[contenthash:8].js';
+            config.output.chunkFilename = 'static/chunks/[contenthash:16].js';
+        }
+        return config;
+    },
 };
 
 module.exports = nextConfig;
